ba61850ba6c9cf7030b6ec4c1334af8a08f2f43a
Davi Lorena <davilorena2016@gmail.com>
2024-09-23 14:29:03 -0300

    Formatting the currency

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
 // Selecionando elementos 
 const amount = document.getElementById("amount")
 
+// Validando o input e o formatando para somente receber números 
 amount.oninput = () => {
-    let value = amount.value.replace(/\D/g, " ")
+    let value = amount.value.replace(/\D/g, "")
 
-amount.value = value.trim()
+    // Transformando em centavos para validar o input, dando uma melhor acessiblidade ao colocar o valor
+    value = Number(value) / 100
+
+    // Formatando o valor do input por meio da function 
+amount.value = formatCurrencyBRL(value)
 }
 
+function formatCurrencyBRL(value) {
+    // Retorna o valor formatado em reais.
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+
+    return value
+}
